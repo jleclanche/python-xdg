6ca1db20a25da01cff5b66c10ad1808ecdc3291b
Jerome Leclanche <jerome@leclan.ch>
2011-12-19 16:44:39 +0000

    Add IniFile.get()

--- xdg.py
+++ xdg.py
@@ -50,6 +50,9 @@ class IniFile(object):
 	def __repr__(self):
 		return self.keys.__repr__()
 
+	def get(self, key, default=None):
+		return self.keys.get(key, default)
+
 	def parse(self, path):
 		with open(path, "r") as file:
 			self.cfg = RawConfigParser()
