2df538d1cbbbccc3886baedade0b782d790eb50c
Jerome Leclanche <jerome@leclan.ch>
2012-04-05 01:22:19 +0100

    autostart: Add stubbed autostart module

---
+++ autostart.py
@@ -0,0 +1,26 @@
+"""
+Desktop Application Autostart Specification
+http://standards.freedesktop.org/autostart-spec/autostart-spec-latest.html
+"""
+
+import os
+from .basedir import XDG_CONFIG_HOME, XDG_CONFIG_DIRS
+
+AUTOSTART_DIRS = [os.path.join(x, "autostart") for path in XDG_CONFIG_DIRS]
+
+def localAutostartPrograms():
+	return []
+
+def globalAutostartPrograms():
+	return []
+
+def allAutostartPrograms():
+	return localAutostartPrograms() + globalAutostartPrograms()
+
+def autorunForMedium(path):
+	for f in [".autorun", "autorun", "autorun.sh"]:
+		path = os.path.join(path, f)
+		if os.path.exists(path):
+			return path
+
+# TODO autoopen files
